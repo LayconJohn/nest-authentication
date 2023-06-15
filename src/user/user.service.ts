f72df0cc611ec752ead8e2fd45c9fad57f03de6d
layconjohn <layconjohn@gmail.com>
2023-06-15 01:35:24 -0300

    feat(user): add find by email on service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -23,8 +23,12 @@ export class UserService {
     };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: {
+        email
+      }
+    })
   }
 
 }
