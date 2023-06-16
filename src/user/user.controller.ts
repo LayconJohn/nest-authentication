edf0ce2fcd9dac8de52f6bcebc92a897f29a804c
layconjohn <layconjohn@gmail.com>
2023-06-15 21:52:30 -0300

    feat(auth): implement '@CurrentUser()'

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { IsPublic } from 'src/auth/decorators/is-public.decorator';
 
 @Controller('user')
 export class UserController {
